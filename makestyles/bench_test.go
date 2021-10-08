e9af2118f4b9a7e23063f6354be5699e6ae22960
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-10-07 17:27:19 -0700

    makestyles: add benchmarks for make

---
+++ makestyles/bench_test.go
@@ -0,0 +1,45 @@
+package makestyles
+
+import "testing"
+
+func makeIt(n int) (data []int) {
+	for i := 0; i < n; i++ {
+		data = append(data, i)
+	}
+	return data
+}
+
+var sink interface{}
+
+func benchmarkMake(b *testing.B, size int) {
+	for i := 0; i < b.N; i++ {
+		sink = makeIt(size)
+	}
+	if sink == nil {
+		b.Fatal("Benchmark didn't run")
+	}
+}
+
+func BenchmarkMake10(b *testing.B) {
+	benchmarkMake(b, 10)
+}
+
+func BenchmarkMake100(b *testing.B) {
+	benchmarkMake(b, 100)
+}
+
+func BenchmarkMake1000(b *testing.B) {
+	benchmarkMake(b, 1000)
+}
+
+func BenchmarkMake10000(b *testing.B) {
+	benchmarkMake(b, 10000)
+}
+
+func BenchmarkMake100000(b *testing.B) {
+	benchmarkMake(b, 100000)
+}
+
+func BenchmarkMake1000000(b *testing.B) {
+	benchmarkMake(b, 1000000)
+}
