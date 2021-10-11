7fca06cf466ba190ab894a52b3a72a18c6004855
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-10-10 23:58:22 -0700

    maps: add benchmarks for clearing maps (#7)

---
+++ maps/map-deletion/bench_test.go
@@ -0,0 +1,57 @@
+package mapclearing
+
+import "testing"
+
+func returnValuesAndClearMap(m map[int]int) (values []int) {
+	for key, value := range m {
+		values = append(values, value)
+		delete(m, key)
+	}
+	return values
+}
+
+func populateMap(n int) map[int]int {
+	m := make(map[int]int, n)
+	for i := 0; i < n; i++ {
+		m[i] = i
+	}
+	return m
+}
+
+var sink interface{}
+
+func BenchmarkIt10(b *testing.B) {
+	benchmarkIt(b, 10)
+}
+
+func BenchmarkIt100(b *testing.B) {
+	benchmarkIt(b, 100)
+}
+
+func BenchmarkIt1000(b *testing.B) {
+	benchmarkIt(b, 1000)
+}
+
+func BenchmarkIt10000(b *testing.B) {
+	benchmarkIt(b, 10000)
+}
+
+func BenchmarkIt100000(b *testing.B) {
+	benchmarkIt(b, 100000)
+}
+
+func BenchmarkIt1000000(b *testing.B) {
+	benchmarkIt(b, 1000000)
+}
+
+func benchmarkIt(b *testing.B, n int) {
+	for i := 0; i < b.N; i++ {
+		m := populateMap(n)
+		sink = returnValuesAndClearMap(m)
+	}
+
+	if sink == nil {
+		b.Fatal("Benchmark did not run")
+	}
+	sink = (interface{})(nil)
+}
